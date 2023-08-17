164053a40c87591497219a15fb71baed8586aed8
Podobnykh Aleksandr <pa@ueba.su>
2023-08-17 16:34:03 +0300

    Initial commit

---
+++ school/views.py
@@ -0,0 +1,16 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    students = Student.objects.all()
+    context = {'object_list': students}
+
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    ordering = 'group'
+
+    return render(request, template, context)
